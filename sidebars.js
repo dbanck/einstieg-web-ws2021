4c85468df2b02483b2a329dbe115bf1ae1eaab12
Daniel Banck <dbanck@users.noreply.github.com>
2021-05-31 19:10:58 +0200

    Add legal notes

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Kurs: ['overview', 'dates', 'projects', 'questions', 'faq'],
-    Notizen: ['notes/general', 'notes/editors', 'notes/devtools', 'notes/git'],
-    // Notizen: ['notes/hosting', 'notes/legal', 'notes/next'],
+    Notizen: ['notes/general', 'notes/editors', 'notes/devtools', 'notes/git', 'notes/legal'],
+    // Notizen: ['notes/hosting', 'notes/next'],
   },
 };
