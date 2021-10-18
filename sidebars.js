f16574b4016bc3b3fd8057534cef31e14feb0982
dbanck <d.banck@gmail.com>
2021-10-18 18:33:31 +0000

    hide content for now

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Kurs: ['overview', 'dates', 'projects', 'questions', 'faq'],
-    Notizen: ['notes/general', 'notes/editors', 'notes/devtools', 'notes/git', 'notes/legal', 'notes/design', 'notes/community', 'notes/next'],
-    // Notizen: ['notes/hosting', ],
+    // Notizen: ['notes/general', 'notes/editors', 'notes/devtools', 'notes/git', 'notes/legal', 'notes/design', 'notes/community', 'notes/next'],
+    Notizen: ['notes/general'],
   },
 };
