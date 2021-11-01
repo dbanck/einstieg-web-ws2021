9106f86af4c49d2b33b4e4b1f8293093a7beb539
Daniel Banck <dbanck@users.noreply.github.com>
2021-11-01 18:50:28 +0100

    enable dev tools note

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Kurs: ['overview', 'dates', 'projects', 'questions', 'faq'],
-    // Notizen: ['notes/general', 'notes/devtools', 'notes/git', 'notes/legal', 'notes/design', 'notes/community', 'notes/next'],
-    Notizen: ['notes/general', 'notes/editors'],
+    // Notizen: ['notes/general',  'notes/git', 'notes/legal', 'notes/design', 'notes/community', 'notes/next'],
+    Notizen: ['notes/general', 'notes/editors', 'notes/devtools'],
   },
 };
